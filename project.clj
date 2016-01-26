f644798f18571ef6ed075a6e2dfa969f30da609e
Henry Cooney <hacoo36@gmail.com>
2016-01-26 14:57:07 -0800

    added project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
+
 (defproject colortron "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
@@ -5,7 +6,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [opencv/opencv "3.1.0"]
-                 [opencv/opencv-native "3.1.0"]]
+                 [opencv/opencv-native "3.1.0"]
+                 [incanter "1.9.0"]]
   :main ^:skip-aot colortron.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
